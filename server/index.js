e656426fd2b5c0c20fbcba4e19ad79562137b607
Matheus Torreão <matheus.bastos@localiza.com>
2021-12-15 12:26:48 -0300

    teste

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!')
 })
+
 
 app.listen(3000, () => console.log("Servidor rodando"))
